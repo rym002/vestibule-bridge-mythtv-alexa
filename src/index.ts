eefe376783ab7402bd94a2182da26fea4338c7e8
rym002 <raymond.munian@gmail.com>
2019-09-24 21:53:36 -0400

    Fix import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { registerModule } from '@vestibule-link/bridge';
-import { registerFrontends } from 'Frontend';
+import { registerFrontends } from './Frontend';
 import { startModule as alexaStartModule } from '@vestibule-link/bridge-assistant-alexa'
 import { startModule as mythtvStartModule } from '@vestibule-link/bridge-mythtv'
 let moduleId: symbol | undefined;
