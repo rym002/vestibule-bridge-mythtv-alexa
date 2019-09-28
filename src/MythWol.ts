318a3a30343abd8870b4fd14e88ab28aa6f2181a
rym002 <raymond.munian@gmail.com>
2019-09-28 00:24:27 -0400

    Handle missing arp command

--- src/MythWol.ts
+++ src/MythWol.ts
@@ -3,6 +3,7 @@ import { CapabilityEmitter } from "@vestibule-link/bridge-assistant-alexa";
 import * as nodeArp from 'node-arp';
 import { promisify } from "util";
 import { MythAlexaEventFrontend } from "./Frontend";
+import { spawn } from "child_process";
 
 type DirectiveType = WakeOnLANController.NamespaceType;
 const DirectiveName: DirectiveType = WakeOnLANController.namespace;
@@ -10,6 +11,7 @@ const DirectiveName: DirectiveType = WakeOnLANController.namespace;
 export default class FrontendWol
     implements CapabilityEmitter {
     readonly getMAC = promisify(nodeArp.getMAC);
+    private readonly spawnCommands = ['arp', 'ping'];
     constructor(readonly fe: MythAlexaEventFrontend) {
         fe.alexaEmitter.on('refreshCapability', this.refreshCapability.bind(this));
     }
@@ -21,8 +23,26 @@ export default class FrontendWol
         const capabilities = await this.capabilities();
         this.fe.alexaEmitter.emit('capability', DirectiveName, capabilities, deltaId);
     }
+    async checkNodeArp(commandName: string):Promise<void> {
+        return new Promise((resolve, reject) => {
+            const arp = spawn('which', [commandName])
+            arp.on('error', (err) => {
+                reject(err)
+            })
+            arp.on('exit', (code, signal) => {
+                if (code) {
+                    reject(new Error('Failed to find ' + commandName))
+                }
+                resolve()
+            })
+        })
+    }
     private async capabilities(): Promise<string[] | undefined> {
         try {
+            const spawns = this.spawnCommands.map(async command=>{
+                await this.checkNodeArp(command)
+            })
+            await Promise.all(spawns)
             const mac = await this.getMAC(this.fe.hostname());
             if (mac) {
                 return [mac];
--- test/MythWol.test.ts
+++ test/MythWol.test.ts
@@ -4,7 +4,6 @@ import * as nodeArp from 'node-arp';
 import { assert, createSandbox, SinonStub } from 'sinon';
 import Handler from '../src/MythWol';
 import { createMockFrontend, verifyRefreshCapability } from './MockHelper';
-
 describe('MythWol', function () {
     const sandbox = createSandbox()
     let getMACStub: SinonStub
@@ -13,14 +12,22 @@ describe('MythWol', function () {
     })
     beforeEach(async function () {
         const frontend = await createMockFrontend('remoteVideo');
-        new Handler(frontend)
+        const handler = new Handler(frontend)
+        const checkNodeStub = sandbox.stub(handler, 'checkNodeArp')
         this.currentTest['frontend'] = frontend
+        this.currentTest['checkNodeStub'] = checkNodeStub
     })
     after(function () {
         sandbox.restore()
     })
     context('Alexa Shadow', function () {
         context('refreshCapability', function () {
+            beforeEach(function () {
+                const checkNodeStub: SinonStub<[string], Promise<void>> = this.currentTest['checkNodeStub']
+                checkNodeStub.callsFake((command) => {
+                    return Promise.resolve()
+                })
+            })
             it('should emit capability if the MAC is found', async function () {
                 const macAddress = '12:34:56:78:90:AB'
                 getMACStub.callsFake((host, cb) => {
@@ -40,6 +47,18 @@ describe('MythWol', function () {
                     cb(new Error('Fake Error'), undefined)
                 })
                 await verifyRefreshCapability(sandbox, this.test['frontend'], true, WakeOnLANController.namespace, undefined)
+            })
+            it('should not emit capability if any command is missing',async function(){
+                const checkNodeStub: SinonStub<[string], Promise<void>> = this.test['checkNodeStub']
+                checkNodeStub.callsFake((command) => {
+                    return Promise.reject(new Error('Command Error ' + command))
+                })
+                const macAddress = '12:34:56:78:90:AB'
+                getMACStub.callsFake((host, cb) => {
+                    cb(undefined, macAddress)
+                })
+                await verifyRefreshCapability(sandbox, this.test['frontend'], true, WakeOnLANController.namespace, undefined)
+                assert.calledTwice(checkNodeStub)
             })
         })
     })
